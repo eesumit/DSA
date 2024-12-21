422abdf559f17a16c8ae9574287c48accc769c98
sumit <yessumitsingh007@gmail.com>
2024-12-21 14:28:55 +0530

    making binary tree

--- TREE/BinaryTree.java
+++ TREE/BinaryTree.java
@@ -1,5 +1,34 @@
 class BinaryTree{
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class BinaryTreeBuilder{
+        static int index=-1;
+        public static Node buildTree(int[] arr){
+            index++;
+            if(arr[index]==-1){
+                return null;
+            }
+            Node newNode = new Node(arr[index]);
+            newNode.left = buildTree(arr);
+            newNode.right = buildTree(arr);
+
+            return newNode;
+        } 
+    }
     public static void main(String[] args) {
-        System.out.println("Hey i am the binary tree");
+        int[] arr = {1,2,3,-1,-1,4,-1,-1,5,-1,6,-1,-1};
+        BinaryTreeBuilder newTree = new BinaryTreeBuilder();
+        Node root = newTree.buildTree(arr);
+        System.out.println(root.data+"  "+"root.left.data="+root.left.data+"root.right.data="+root.right.data);
     }
 }

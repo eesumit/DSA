b09812b4d582a8f0909dfa3b8491da1b6a7da30b
sumit <yessumitsingh007@gmail.com>
2024-12-23 09:54:14 +0530

    making all the functions

--- TREE/BinarySearchTree.java
+++ TREE/BinarySearchTree.java
@@ -1,35 +1,64 @@
-class BinarySearchTree{
-    static class Node{
+class BinarySearchTree {
+    static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
-            this.data=data;
+
+        Node(int data) {
+            this.data = data;
         }
     }
-    private static Node insertInBinarySearchTree(Node root,int data){
-        if(root==null) return new Node(data);
-        else if(root.data>data) root.left = insertInBinarySearchTree(root.left,data);
-        else root.right = insertInBinarySearchTree(root.right, data);
+
+    private static Node insertInBinarySearchTree(Node root, int data) {
+        if (root == null)
+            return new Node(data);
+        else if (root.data > data)
+            root.left = insertInBinarySearchTree(root.left, data);
+        else
+            root.right = insertInBinarySearchTree(root.right, data);
         return root;
     }
-    private static void inorderPrint(Node root){
-        if(root==null) return;
+
+    private static void inorderPrint(Node root) {
+        if (root == null)
+            return;
         inorderPrint(root.left);
-        System.out.println(root.data);
+        System.out.print(root.data+" ");
         inorderPrint(root.right);
     }
+
     private static boolean searchInBinaryTree(Node root, int key) {
-        if (root == null) return false; // If root is null, key is not present
-        if (root.data == key) return true; // Key found
-        if (key < root.data) return searchInBinaryTree(root.left, key); // Key may be in the left subtree
+        if (root == null)
+            return false; // If root is null, key is not present
+        if (root.data == key)
+            return true; // Key found
+        if (key < root.data)
+            return searchInBinaryTree(root.left, key); // Key may be in the left subtree
         return searchInBinaryTree(root.right, key); // Key may be in the right subtree
     }
-    
+
+    private static Node deleteNode(Node root,int key){
+        if(root==null) return null;
+        else if(root.data<key) root.right = deleteNode(root.right, key);
+        else if(root.data>key) root.left = deleteNode(root.left, key);
+        else {
+            if(root.left==null) return root.right;
+            else if(root.right==null) return root.left;
+            Node successor = findSuccessor(root);
+            root.data = successor.data;
+            root.right = deleteNode(root.right, successor.data);
+        }
+        return root;
+    }
+    private static Node findSuccessor(Node root){
+        while(root.left!=null) root=root.left;
+        return root;
+    }
     public static void main(String[] args) {
-        int []arr = {5,1,3,4,2,7};
+        int[] arr = {8,5,3,1,4,6,10,11,14};
         Node root = null;
-        for(int i:arr) root = insertInBinarySearchTree(root, i);
+        for (int i : arr)
+            root = insertInBinarySearchTree(root, i);
         inorderPrint(root);
         boolean kyaMila = searchInBinaryTree(root, 7);
         System.out.println(kyaMila);
@@ -37,7 +66,10 @@ class BinarySearchTree{
         System.out.println(kyaMila);
         kyaMila = searchInBinaryTree(root, 1);
         System.out.println(kyaMila);
-        
+        root = deleteNode(root, 4);
+        root = deleteNode(root, 10);
+        root = deleteNode(root, 5);
+        inorderPrint(root);
 
     }
 }
